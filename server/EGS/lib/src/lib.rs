01b1898b77541f74e348e2f1643df5ed0a0701e8
Jiale Zhang <zhangjiale@linux.alibaba.com>
2022-08-11 22:42:54 -0400

    Verify evidence: Verify the integrity of eventlog.
    Jiale Zhang <zhangjiale@linux.alibaba.com>

--- server/EGS/lib/src/lib.rs
+++ server/EGS/lib/src/lib.rs
@@ -2,6 +2,7 @@ use anyhow::{anyhow, Result};
 use eventlog_rs::Eventlog;
 use librats_rs::{TeeType, get_quote, verify_quote};
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
 use std::fs;
 
@@ -47,10 +48,28 @@ pub async fn verify_evidence(
         return Err(anyhow!("Unsupported TEE type"));
     }
 
-    let _parsed_claims = verify_quote(&evidence.quote, report_data, TeeType::TDX).await?;
+    let parsed_claims = verify_quote(&evidence.quote, report_data, TeeType::TDX).await?;
 
     let event_log = Eventlog::try_from(evidence.eventlog_data.clone())?;
-    // TODO: Verify the integrity of eventlog.
+    
+    let claims = serde_json::from_str::<serde_json::Value>(&parsed_claims)?;
+    let mut rtmrs_from_quote: HashMap<u32, Vec<u8>> = HashMap::new();
+
+    rtmrs_from_quote.insert(0 as u32, base64::decode(claims["rtmr0"].to_string())?);
+    rtmrs_from_quote.insert(1 as u32, base64::decode(claims["rtmr1"].to_string())?);
+    rtmrs_from_quote.insert(2 as u32, base64::decode(claims["rtmr2"].to_string())?);
+    rtmrs_from_quote.insert(3 as u32, base64::decode(claims["rtmr3"].to_string())?);
+
+    let rtmrs_from_eventlog = event_log.replay_measurement_regiestry();
+
+    for rtmr_index in 0..4 {
+        let reference_rtmr = rtmrs_from_quote.get(&rtmr_index).ok_or(anyhow!("Internal Error: get rtmr reference value failed"))?;
+        let eventlog_rtmr = rtmrs_from_eventlog.get(&rtmr_index).ok_or(anyhow!("Internal Error: get rtmr value failed"))?;
+
+        if reference_rtmr != eventlog_rtmr {
+            return Err(anyhow!("RTMR value not matched. Maybe the eventlog has been tampered!!!"));
+        }
+    }
 
     Ok(event_log)
 }
